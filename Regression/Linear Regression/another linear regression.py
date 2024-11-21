51a0d1d40832e6926e9e3e6e712edbe8260fd1dc
Utku Kurabiye <egeutkurenk@gmail.com>
2024-11-21 23:22:15 +0300

    linear regression exercise commit

---
+++ Regression/Linear Regression/another linear regression.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = pd.read_csv('Databases/Salary_Data.csv')
+
+x = data.iloc[:, :-1].values
+y = data.iloc[:, -1].values
+
+#train - test sets split
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
+#linear regression model
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(x_train, y_train)
+
+#prediction
+y_prediction = regressor.predict(x_test)
+
+#visuals train set
+plt.scatter(x_train, y_train, color = 'red')
+plt.plot(x_train, regressor.predict(x_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+#test set
+plt.scatter(x_test, y_test, color = 'red')
+plt.plot(x_train, regressor.predict(x_train), color = 'blue')
+plt.title('Salary vs Experience (Training set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
