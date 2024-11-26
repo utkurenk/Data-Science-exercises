914480dcc8dd805cd9478dbfbee9fc6c970e9d2b
Utku Kurabiye <egeutkurenk@gmail.com>
2024-11-27 00:12:13 +0300

    random tree regression

---
+++ Regression/random forest regression.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+data = pd.read_csv('Databases/Position_Salaries.csv')
+
+x = data.iloc[:, 1:-1].values
+y = data.iloc[:, -1].values
+
+#regression
+from sklearn.ensemble import RandomForestRegressor
+regressor = RandomForestRegressor(random_state = 0)
+regressor.fit(x, y)
+
+#predict new result
+print(regressor.predict([[6.5]]))
+
+#visuals
+x_grid = np.arange(min(x), max(x), 0.1)
+x_grid = x_grid.reshape((len(x_grid), 1))
+plt.scatter(x, y, color = 'red')
+plt.plot(x_grid, regressor.predict(x_grid), color = 'blue')
+plt.title('Truth or Bluff (Decision Tree)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
